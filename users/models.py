2fc6f3024abce6bf193c76e35a7a2b3b8444d4df
testsaccountsss <gapr0317@gmail.com>
2024-12-24 13:32:18 +0900

    💡 chore: 코드 린트 적용

--- users/models.py
+++ users/models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
+
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError('이메일은 필수 값입니다.')
+            raise ValueError("이메일은 필수 값입니다.")
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -12,10 +13,11 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('role', 'WORKSHOP')  # 기본값 설정
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("role", "WORKSHOP")  # 기본값 설정
         return self.create_user(email, password, **extra_fields)
+
 
 class User(AbstractUser):
     # AbstractUser의 기본 필드 중 사용하지 않을 필드 무효화
@@ -24,10 +26,10 @@ class User(AbstractUser):
     last_name = None
 
     class Meta:
-        db_table = 'users'
-        verbose_name = '사용자'
-        verbose_name_plural = '사용자 목록'
-        ordering = ['-created_at']
+        db_table = "users"
+        verbose_name = "사용자"
+        verbose_name_plural = "사용자 목록"
+        ordering = ["-created_at"]
 
     objects = UserManager()
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
