989cd21dc11be9de250f466125121fd0965f32e9
testsaccountsss <gapr0317@gmail.com>
2024-12-24 13:09:54 +0900

    ✨ feat: 유저매니저 구현
    - Add create_user method
    - Add create_superuser method
    - Set email as required field"

--- users/models.py
+++ users/models.py
@@ -1,7 +1,24 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('이메일은 필수 값입니다.')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('role', 'WORKSHOP')  # 기본값 설정
+        return self.create_user(email, password, **extra_fields)
+
 class User(AbstractUser):
+    # AbstractUser의 기본 필드 중 사용하지 않을 필드 무효화
     username = None
     first_name = None
     last_name = None
@@ -11,3 +28,6 @@ class User(AbstractUser):
         verbose_name = '사용자'
         verbose_name_plural = '사용자 목록'
         ordering = ['-created_at']
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
