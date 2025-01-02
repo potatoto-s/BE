e8106f6bf83c3f24719e9589926c6a6025c9ca89
seungwon0113 <jungseungwon0113@gmail.com>
2025-01-02 13:28:07 +0900

    ✨ feat: added cors method

--- config/settings/base.py
+++ config/settings/base.py
@@ -82,6 +82,15 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:5173",
 ]
 
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
